c94db70b59ec7d2b7ec275fc3dcdad7bb278ade1
Ralf S. Engelschall <rse@engelschall.com>
2020-05-16 17:32:03 +0200

    add Howler loop flag

---
+++ soundlp.data-sprite.js
@@ -0,0 +1,35 @@
+/*
+**  SoundLP -- Sound Loop Collection
+**  Copyright (c) 2020 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**
+**  Permission is hereby granted, free of charge, to any person obtaining
+**  a copy of this software and associated documentation files (the
+**  "Software"), to deal in the Software without restriction, including
+**  without limitation the rights to use, copy, modify, merge, publish,
+**  distribute, sublicense, and/or sell copies of the Software, and to
+**  permit persons to whom the Software is furnished to do so, subject to
+**  the following conditions:
+**
+**  The above copyright notice and this permission notice shall be included
+**  in all copies or substantial portions of the Software.
+**
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+**  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+**  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+**  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+**  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+**  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+**  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+const fs = require("fs")
+
+;(async () => {
+    let data = JSON.parse(fs.readFileSync("soundlp.data-sprite.json", { encoding: "utf8" }))
+    for (const name of Object.keys(data.sprite))
+        data.sprite[name].push(true)
+    fs.writeFileSync("soundlp.data-sprite.json", JSON.stringify(data, null, "  "), { encoding: "utf8" })
+})().catch((err) => {
+    console.log(`ERROR: ${err}`)
+})
+
